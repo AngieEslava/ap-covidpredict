50b9d800e0d279cbd19ea169d40d2351173f9bad
AngieEslava <egangiest@gmail.com>
2020-09-07 23:05:38 -0500

    changing setup

--- setup.sh
+++ setup.sh
@@ -1,9 +1,13 @@
 mkdir -p ~/.streamlit/
 
 echo "\
+[general]\n\
+email = \"egangiest@gmail.com\"\n\
+" > ~/.streamlit/credentials.toml
+
+echo "\
 [server]\n\
-port = $PORT\n\
-enableCORS = True\n\
 headless = true\n\
-\n\
+enableCORS = false\n\
+port = $PORT\n\
 " > ~/.streamlit/config.toml
